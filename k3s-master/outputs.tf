77bd53c3715cec915b51cbbba62eba7108e86baa
OleksDovz <oleksdovz@gmail.com>
2024-06-24 14:31:57 +0300

    fix(DMP-000):  Update file: k3s-master/outputs.tf

--- k3s-master/outputs.tf
+++ k3s-master/outputs.tf
@@ -12,14 +12,9 @@ output "vm_ipv4_addresses" {
   description = "vm_ipv4_addresses"
   value = module.vms.vm_ipv4_addresses
 }
-#
-# output "config" {
-#   description = "config"
-#   value = module.k3s-master.k3s_config
-# }
-#
 
-output "ssh_private_key" {
-  description = "ssh_private_key"
-  value = module.k3s-master.ssh_private_key
+output "config" {
+  description = "config"
+  value = module.k3s-master.k3s_config
 }
+
