2b263c3fd2dfe5188e99daf725437b69a7d3864a
OleksDovz <oleksdovz@gmail.com>
2024-06-24 10:50:49 +0300

    fix(DMP-000): D nginx-vm/main.tf  Add file: vm/main.tf R nginx-vm/outputs.tf -> vm/outputs.tf R nginx-vm/variables.tf -> vm/variables.tf R nginx-vm/version.tf -> vm/version.tf

---
+++ vm/main.tf
@@ -0,0 +1,19 @@
+data "local_file" "key" {
+  filename = pathexpand("${var.ssh_public_key}")
+}
+
+
+
+module "vms" {
+  source         = "git@github.com:oleksdovz/terraform-proxmox-modules.git//proxmox-vm?ref=main"
+  vm_name        = var.vm_name
+  vm_id          = var.vm_id
+  vm_username    = var.vm_username
+  vm_memory      = var.vm_memory
+  vm_cpu_cores   = var.vm_cpu_cores
+  qcow_url       = var.qcow_url
+  vm_networking  = var.vm_networking
+  vm_dns_servers = var.vm_dns_servers
+  ssh_public_key = data.local_file.key.content
+  custom_script  = var.custom_script
+}
