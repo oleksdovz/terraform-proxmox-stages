fed88dcbf422badd01beb54f9e290d37ca2e4a95
OleksDovz <oleksdovz@gmail.com>
2024-06-24 12:11:52 +0300

    fix(DMP-000):  Update file: vm/main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -4,17 +4,32 @@ data "local_file" "key" {
 
 
 module "vms" {
-  source         = "git@github.com:oleksdovz/terraform-proxmox-modules.git//proxmox-vm?ref=main"
-  environment    = var.environment
-  vm_name        = var.vm_name
-  vm_id          = var.vm_id
-  vm_username    = var.vm_username
-  vm_memory      = var.vm_memory
-  vm_cpu_cores   = var.vm_cpu_cores
-  qcow_url       = var.qcow_url
-  vm_networking  = var.vm_networking
-  vm_dns_servers = var.vm_dns_servers
-  ssh_public_key = data.local_file.key.content
-  custom_script  = var.custom_script
+  source                = "git@github.com:oleksdovz/terraform-proxmox-modules.git//proxmox-vm?ref=main"
+  ssh_public_key        = data.local_file.key.content
+  node_name             = var.node_name
+  environment           = var.environment
+  iso_datastore_id      = var.iso_datastore_id
+  vm_datastore_id       = var.vm_datastore_id
+  qcow_url              = var.qcow_url
+  vm_username           = var.vm_username
+  vm_password           = var.vm_password
+  vm_name               = var.vm_name
+  vm_id                 = var.vm_id
+  custom_script         = var.custom_script
+  vm_startup_order      = var.vm_startup_order
+  vm_startup_up_delay   = var.vm_startup_up_delay
+  vm_startup_down_delay = var.vm_startup_down_delay
+  vm_dns_domain         = var.vm_dns_domain
+  vm_dns_servers        = var.vm_dns_servers
+  vm_networking         = var.vm_networking
+  vm_memory             = var.vm_memory
+  vm_memory_floating    = var.vm_memory_floating
+  vm_memory_shared      = var.vm_memory_shared
+  vm_cpu_architecture   = var.vm_cpu_architecture
+  vm_cpu_cores          = var.vm_cpu_cores
+  vm_cpu_sockets        = var.vm_cpu_sockets
+  vm_cpu_type           = var.vm_cpu_type
+  vm_disk_size          = var.vm_disk_size
+  vm_os_config          = var.vm_os_config
 
 }
